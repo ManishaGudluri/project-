e9dae4b45c9509ab6e6e82fb2f2add2a4858b52d
ManishaGudluri <manishagudluri40@gmail.com>
2025-07-08 12:14:16 +0530

    Add files via upload

---
+++ DemoSpringMvc3Application.java
@@ -0,0 +1,13 @@
+package com.cg.DemoSpringMVC3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringMvc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringMvc3Application.class, args);
+	}
+
+}
